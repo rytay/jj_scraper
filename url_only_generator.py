43f98aa65b46babb7ca6ffb20eb3bf80d977c412
Ryan <rytay123@gmail.com>
2020-03-27 06:19:14 -0400

    fixed url only parsing

--- url_only_generator.py
+++ url_only_generator.py
@@ -17,14 +17,13 @@ import re
 from bs4 import BeautifulSoup
 
 keywords = set(open("keywords.txt").read().splitlines())
-date = datetime.now()
+date = str(datetime.now())
 
 
-class ContractUrlCrawler(CrawlSpider):
+class ContractCrawlerUrl(CrawlSpider):
     
     #keyword file
     keywords = set(open("keywords.txt").read().splitlines())
-    urls = []
     date = str(datetime.now())
     custom_settings = {
         'LOG_LEVEL' : 'INFO',
@@ -34,22 +33,35 @@ class ContractUrlCrawler(CrawlSpider):
         'RETRY_ENABLED': 'False'
     }
 
-    name = "contract_url_crawler"
+    name = "contract_crawler_url"
     allowed_domains = ['buyandsell.gc.ca']
     start_urls = ["https://buyandsell.gc.ca/procurement-data/search/site?f%5B0%5D=sm_facet_procurement_data%3Adata_data_tender_notice&f%5B1%5D=ss_publishing_status%3ASDS-SS-005"]
     rules = (Rule(LinkExtractor(restrict_xpaths=(['//ul[@class="search-results"]//h2/a','//li[@class="pager-next"]/a'])),callback='parse_item',follow=True),)
     
-def parse_item(self, response: HtmlResponse):
+    def parse_item(self, response: HtmlResponse):
         print(response.url)
         if "search/site" not in response.url:
             soup = BeautifulSoup(response.text,'html.parser')
             article_text = soup.find("article", {"class":"data-table"}).get_text()
-            
             if any(keyword in article_text for keyword in self.keywords):
-                with open(self.date+'.txt','w') as f:
+                with open(self.date+'.txt', 'w') as f:
                     f.write(response.url+'\n\n')
-        return scrapy.Item()
+                yield
+
+def checkNone(item):
+    if item is None:
+        return 'none'
+    else:
+        return item.get_text()
+
 
 process = CrawlerProcess()
-process.crawl(ContractUrlCrawler)
+process.crawl(ContractCrawlerUrl)
 process.start()
+
+#Pretty print for readability
+'''
+with open('results.json' ,'w+') as f:
+    data=json.load(f)
+    f.write(json.dumps(data, indent=1))
+'''
